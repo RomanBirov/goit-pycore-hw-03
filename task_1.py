fc52a6226fad7bb577b780644fb3996c884566fc
RomanBirov <roman0630565956@gmail.com>
2025-10-08 21:08:39 +0300

    use try/except for incorect value

--- task_1.py
+++ task_1.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 
 def get_days_from_today(date: str) -> int:
-    enter_date = datetime.strptime(date, "%Y-%m-%d").date()
-    today_date = datetime.today().date()
-    diff_between_date = today_date - enter_date
-    return diff_between_date.days
+    try:
+        enter_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today_date = datetime.today().date()
+        diff_between_date = today_date - enter_date
+        return diff_between_date.days
+    except ValueError:
+        print("Incorrect date format. Please use the 'YYYY-MM-DD' format.")
+        return 0
+print(get_days_from_today("13.14.2020"))
